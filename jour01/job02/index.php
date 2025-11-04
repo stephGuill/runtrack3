035df836a3ce2f59d9aa1764491a6d75e4a2c885
stephGuill <stephane.guillamo@laplateforme.io>
2025-11-04 14:14:30 +0100

    jour01/job02

---
+++ jour01/job02/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hello Javascript</title>
+        <!-- inclusion du fichier Javascript externe -->
+        <!-- l'attribut src indique le chemin relatif vers le fichier script.js -->
+        <!-- cette balise <script> avec src charge un fichier Javascript externe -->
+        <!-- le navigateur téléchargera automatiquement le fichier script.js -->
+        <!-- et executera son contenu dans le contexte de cette page HTML -->
+         <script src="script.js"></script>
+    </head>
+<body>
+</body>
+</html>
---
+++ jour01/job02/script.js
@@ -0,0 +1,11 @@
+// fichier Javascript externe
+// fichier qui est séparé du html et inclus via la balise <script src="script.js">
+// contient le code Javascript qui sera éxecuté par le navigateur
+ 
+// cette fonction affiche une boîte de dialogue (popup) avec un message
+// alert() est une fonction Javascript native (intégrée au navigateur)
+// elle affiche une fenêtre popup modale avec le message spécifié
+// "modale" signifie que l'utilisateur doit fermer le popup pour continuer
+// "Hello Javascript!" est une chaîne de caractères (string) passé en paramètre
+// la popup aura un bouton "ok" que l'utilisateur doit cliquer
+alert("Hello Javascript");
