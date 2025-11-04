607ed8cfed6ce74cab96a471eb54483c9657bf08
stephGuill <stephane.guillamo@laplateforme.io>
2025-11-04 13:42:01 +0100

    ex jour01/job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hello Javascript</title>
+        <script>
+            alert("Hello Javascript!");
+        </script>
+    </head>
+
+    <body>
+    </body>
+    </html>
